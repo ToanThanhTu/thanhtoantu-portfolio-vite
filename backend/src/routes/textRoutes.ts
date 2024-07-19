5130b66537d6b584351392fc7db6ac50d54ef630
Thanh Toan Tu <toantu3005@gmail.com>
2024-07-19 23:23:57 +1000

    remove frontend and backend before add them back as submodules

--- backend/src/routes/textRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAllTexts, createText } from '../controllers/textControllers';
-
-const textRouter = Router();
-
-textRouter.get('/texts', getAllTexts);
-textRouter.post('/texts', createText);
-
-export default textRouter;
--- backend/src/routes/weatherRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getWeather } from "../controllers/weatherController";
-
-const weatherRouter = Router();
-
-weatherRouter.get('/', getWeather);
-
-export default weatherRouter;
